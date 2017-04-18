7686a55e433a872d7735de26f4a5e365342d5112
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-04-19 08:33:09 +0900

    Add initial env for anyenv

---
+++ conf.d/0_initial_env.fish
@@ -0,0 +1,17 @@
+# Setup for anyenv
+set -x PATH $PATH $HOME/.anyenv/bin
+
+# Setup *env managed by anyenv
+set -l envs (anyenv envs)
+for e in $envs
+  set -l upper (echo $e | tr a-z A-Z)
+  set -l root (anyenv root)/envs/$e
+
+  set -x "$upper"_ROOT $root
+  set -x "$upper"_SHELL fish
+  set -x PATH $PATH $root/bin
+end
+
+# Setup Go using goenv
+set -x GOPATH $HOME/dev
+set -x PATH $PATH $GOENV_ROOT/bin $GOPATH/bin
