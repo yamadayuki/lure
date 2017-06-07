30b91afe243a43030162eac14bbb290f9b773a1e
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-06-07 09:39:06 +0900

    Add browse on github command

---
+++ functions/__fzf_browse_github.fish
@@ -0,0 +1,15 @@
+function __fzf_browse_github -d "Browse remote repository on github"
+  set =l query (commandline)
+
+  if test -n query
+    set fzf_flags -q "$query"
+  end
+
+  ghq list | fzf $fzf_flags | read line
+
+  if [ $line ]
+    set -l repo_name (string split -m1 "/" $line)[2]
+    hub browse $repo_name
+    commandline -f repaint
+  end
+end
--- key_bindings.fish
+++ key_bindings.fish
@@ -1,6 +1,8 @@
 bind \ct '__fzf_go_src'
 bind \cg '__fzf_code'
+bind \cb '__fzf_browse_github'
 if bind -M insert >/dev/null ^/dev/null
     bind -M insert \ct '__fzf_go_src'
     bind -M insert \cg '__fzf_code'
+    bind -M insert \cb '__fzf_browse_github'
 end
