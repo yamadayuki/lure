2c8bcd6e44914a66e5d8f72b5e25c7f2c60de17a
Yuki Yamada <yamadayuuki0818@gmail.com>
2018-03-27 22:07:05 +0900

    Fix anyenv directive

--- conf.d/0_initial_env.fish
+++ conf.d/0_initial_env.fish
@@ -1,15 +1,17 @@
 # Setup for anyenv
 set -x PATH $PATH $HOME/.anyenv/bin
 
-# Setup *env managed by anyenv
-set -l envs (anyenv envs)
-for e in $envs
-  set -l upper (echo $e | tr a-z A-Z)
-  set -l root (anyenv root)/envs/$e
+if [ which anyenv ]
+  # Setup *env managed by anyenv
+  set -l envs (anyenv envs)
+  for e in $envs
+    set -l upper (echo $e | tr a-z A-Z)
+    set -l root (anyenv root)/envs/$e
 
-  set -x "$upper"_ROOT $root
-  set -x "$upper"_SHELL fish
-  set -x PATH $PATH $root/bin
+    set -x "$upper"_ROOT $root
+    set -x "$upper"_SHELL fish
+    set -x PATH $PATH $root/bin
+  end
 end
 
 # Setup Go using goenv
