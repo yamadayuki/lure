13ce31e3f11c84db8aa1153da64878980d89dc7e
Yuki Yamada <yamadayuuki0818@gmail.com>
2018-10-09 15:32:32 +0900

    Do not use read 0

--- functions/__fzf_go_src.fish
+++ functions/__fzf_go_src.fish
@@ -5,7 +5,7 @@ function __fzf_go_src
         set fzf_flags -q "$query"
     end
 
-    ghq list | eval (__fzfcmd) --read0 --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' | perl -pe 'chomp if eof' | read line
+    ghq list | eval (__fzfcmd) --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' | perl -pe 'chomp if eof' | read line
 
     if [ $line ]
         ghq root | read dir
