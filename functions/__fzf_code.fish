255dfaae3f7ae283819171c23b9a6a816afb29ec
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-05-10 12:05:32 +0900

    Modify fzf command to list repositories

--- functions/__fzf_code.fish
+++ functions/__fzf_code.fish
@@ -5,7 +5,7 @@ function __fzf_code
         set fzf_flags -q "$query"
     end
 
-    ghq list | fzf $fzf_flags | read line
+    ghq list | eval (__fzfcmd) --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' | perl -pe 'chomp if eof' | read line
 
     if [ $line ]
         ghq root | read dir
