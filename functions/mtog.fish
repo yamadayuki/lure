16d7ba861b2bcbad3d7108637e8f829b2bbddee2
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-05-28 17:19:31 +0900

    Tweak brightness

--- functions/mtog.fish
+++ functions/mtog.fish
@@ -1,3 +1,3 @@
 function mtog
-    ffmpeg -i $argv[1] -vf scale=800:-1 -r 10 -f image2pipe -vcodec ppm - | convert -delay 5 -layers Optimize -loop 0 - $argv[2]
+    ffmpeg -i $argv[1] -vf scale=800:-1 -vf eq=brightness=0.05 -r 10 -f image2pipe -vcodec ppm - | convert -delay 5 -layers Optimize -loop 0 - $argv[2]
 end
