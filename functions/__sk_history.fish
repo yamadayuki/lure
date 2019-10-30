2b2e2dcd6e5511e37bc5bbbd75da6e83c6bdf4ab
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-10-30 16:04:13 +0900

    Add query for __sk_history

--- functions/__sk_history.fish
+++ functions/__sk_history.fish
@@ -1,6 +1,7 @@
 function __sk_history
+    set -l query (commandline)
     history merge
-    history -z | sk --read0 $lure_skim_default_option | perl -pe 'chomp if eof' | read -lz result
+    history -z | sk --read0 $lure_skim_default_option -q $query | perl -pe 'chomp if eof' | read -lz result
     and commandline -- $result
     commandline -f repaint
 end
