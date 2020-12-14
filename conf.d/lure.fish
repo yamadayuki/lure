181def071db1c94fe278979b2ca0f5bec5a198b9
Yamada Yuki <yamadayuuki0818@gmail.com>
2020-12-14 11:31:23 +0900

    Update fish command colors

--- conf.d/lure.fish
+++ conf.d/lure.fish
@@ -4,7 +4,7 @@ function _lure_set_default -S -a var default
     end
 end
 
-# Colors
+# Prompt Colors
 _lure_set_default lure_color_primary (set_color blue)
 _lure_set_default lure_color_info (set_color cyan)
 _lure_set_default lure_color_mute (set_color brblack)
@@ -30,6 +30,42 @@ _lure_set_default lure_color_git_dirty $lure_color_mute
 
 _lure_set_default lure_color_command_duration $lure_color_warning
 _lure_set_default lure_color_current_time $lure_color_success
+
+# Command Colors
+set --global fish_color_normal brwhite
+# set --global fish_color_autosuggestion brblack
+# set --global fish_color_cancel         brcyan
+set --global fish_color_command brblue
+set --global fish_color_comment brblack
+# set --global fish_color_cwd            brred
+# set --global fish_color_end            brwhite
+set --global fish_color_error brred
+# set --global fish_color_escape         brcyan
+# set --global fish_color_host           brgreen
+# set --global fish_color_host_remote    bryellow
+# set --global fish_color_match          brcyan --underline
+set --global fish_color_operator brcyan
+set --global fish_color_param brgreen
+set --global fish_color_quote bryellow
+set --global fish_color_redirection brcyan
+# set --global fish_color_search_match   --background=brblack
+# set --global fish_color_selection      --background=brblack
+# set --global fish_color_user           brblue
+
+set --global fish_pager_color_progress brcyan --reverse
+# set --global fish_pager_color_background
+set --global fish_pager_color_prefix brwhite --underline
+set --global fish_pager_color_completion brwhite
+set --global fish_pager_color_description bryellow
+# set --global fish_pager_color_secondary_background  # null
+# set --global fish_pager_color_secondary_completion  brblack
+# set --global fish_pager_color_secondary_description brblack
+# set --global fish_pager_color_secondary_prefix      brblack
+# set --global fish_pager_color_selected_background   --background=brblack
+# set --global fish_pager_color_selected_completion   bryellow
+# set --global fish_pager_color_selected_description  bryellow
+# set --global fish_pager_color_selected_prefix brblack
+
 
 # Global variables
 set --global lure_skim_default_option --tiebreak=index --inline-info
