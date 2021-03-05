18e5ba5bded8e539c68962d924568d7ed56156fe
Yamada Yuki <yamadayuuki0818@gmail.com>
2021-03-05 10:56:08 +0900

    Tweak

--- functions/_lure_node_version.fish
+++ functions/_lure_node_version.fish
@@ -1,5 +1,5 @@
 function _lure_node_version
-    set -l node_version (nodenv version-name 2>/dev/null)
+    set -l node_version (node -v)
 
     echo "$lure_color_node_version$lure_symbol_node_version $node_version"
 end
--- functions/_lure_prompt_first_line.fish
+++ functions/_lure_prompt_first_line.fish
@@ -1,3 +1,3 @@
 function _lure_prompt_first_line
-    echo (_lure_print_prompt (_lure_current_directory) (_lure_prompt_git) (_lure_node_version))
+    echo (_lure_print_prompt (_lure_current_directory) (_lure_prompt_git) # (_lure_node_version) (_lure_rust_version))
 end
