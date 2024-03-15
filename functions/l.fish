3d09dc85d14faaae87369b5cf503301013682613
Yamada Yuki <yamadayuuki0818@gmail.com>
2024-03-15 19:25:39 +0900

    Use eza instead of exa

--- functions/l.fish
+++ functions/l.fish
@@ -1,3 +1,3 @@
 function l
-    exa --long --all --header --octal-permissions --time-style=long-iso --git $argv
+    eza --long --all --header --octal-permissions --time-style=long-iso --git $argv
 end
